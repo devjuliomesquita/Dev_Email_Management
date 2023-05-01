0a9a000b4eb9f41bfb3d277b90e21f6a9149f2f4
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-05-01 01:15:46 -0300

    Implementado as demais interfaces de serviço

---
+++ Dev_Email_Management.Domain/Services/BusinessStateService.cs
@@ -0,0 +1,18 @@
+﻿using Dev_Email_Management.Domain.Entities;
+using Dev_Email_Management.Domain.Interfaces.Repositories;
+using Dev_Email_Management.Domain.Interfaces.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev_Email_Management.Domain.Services
+{
+    public class BusinessStateService : ServiceBase<BusinessState>, IBusinessStateService
+    {
+        public BusinessStateService(IRepositoryBase<BusinessState> repository) : base(repository)
+        {
+        }
+    }
+}
